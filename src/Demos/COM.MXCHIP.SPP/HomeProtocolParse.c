798d89bd7de5e32ddc1d03f657a8ccf119d17151
Joekey <jiabao1985@gmail.com>
2015-04-22 16:28:13 +0800

    添加json字符串

--- src/Demos/COM.MXCHIP.SPP/HomeProtocolParse.c
+++ src/Demos/COM.MXCHIP.SPP/HomeProtocolParse.c
@@ -7,8 +7,13 @@
 #include <stdio.h>
 #include <string.h>
 #include "SparkLight.h"
+#include "JSON-C/json.h"
+
 #define home_log(M, ...) custom_log("SPP", M, ##__VA_ARGS__)
 #define home_log_trace() custom_log_trace("SPP")
+
+
+
 
 
 /*
@@ -29,6 +34,7 @@ extern void HomeSwitch1Control(bool);
 extern void HomeSwitch2Control(bool);
 
 
+
 OSStatus HomeWlanCommandProcess(unsigned char *inBuf, int *inBufLen, int inSocketFd, mico_Context_t * const inContext)
 {
   home_log_trace();
@@ -36,10 +42,18 @@ OSStatus HomeWlanCommandProcess(unsigned char *inBuf, int *inBufLen, int inSocke
   (void)inContext;
   OSStatus err = kUnknownErr;
 
+ 
+ json_object* json_buf;
+json_buf= json_object_new_object();
+json_object_object_add(json_buf,"cmd",json_object_new_string("0x0b01"));
+const char* strret = json_object_to_json_string(json_buf);
+
+ MicoUartSend(UART_FOR_APP,strret,strlen(strret));
   if(memcmp(inBuf,cmdOpen,strlen(cmdOpen))==0){
 
+	
 	memset(sendBuffer,0,256);
-	LightCfgPackage(SRC_SUBNET_ID, SRC_DEV_ID, 1, 17, 1,100);
+	//LightCfgPackage(SRC_SUBNET_ID, SRC_DEV_ID, 1, 17, 1,100);
 	//err = MicoUartSend(UART_FOR_APP,tempAckOpen, strlen(tempAckOpen));
 	//add by jacky for smarthome
 	HomeSwitch1Control(TRUE);
@@ -47,7 +61,7 @@ OSStatus HomeWlanCommandProcess(unsigned char *inBuf, int *inBufLen, int inSocke
   }else if(memcmp(inBuf,cmdClose,strlen(cmdClose))==0){
 
 	memset(sendBuffer,0,256);
-	LightCfgPackage(SRC_SUBNET_ID, SRC_DEV_ID, 1, 17, 1,0);
+	//LightCfgPackage(SRC_SUBNET_ID, SRC_DEV_ID, 1, 17, 1,0);
 	//err = MicoUartSend(UART_FOR_APP, tempAckClose, strlen(tempAckClose));
 	//add by jacky for smarthome
 	HomeSwitch1Control(FALSE);
